9f0dcf6da258d7ef5b01a8518c2fc0d4d3ff6197
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-06-10 22:26:14 +0530

    add utility functions

---
+++ api/src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+// This is Promise Handler with and advance syntax.
+
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export default asyncHandler;
