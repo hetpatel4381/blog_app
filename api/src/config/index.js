6763a9ed635b711708ed54e249dd2d05277c7ad9
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-06-11 16:19:59 +0530

    create sign in api route

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 const _config = {
   mongo_uri: process.env.MONGO_URI,
   port: process.env.PORT,
+  jwt_key: process.env.JWT_SECRET_KEY,
 };
 
 export const config = Object.freeze(_config);
--- api/src/routes/auth.route.js
+++ api/src/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const signupRouter = express.Router();
 
 signupRouter.post("/signup", signup);
+signupRouter.post("/signin", signin);
 
 export default signupRouter;
