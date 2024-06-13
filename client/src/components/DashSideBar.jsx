6ae445aee637d8e97cee810e4d8f21f1cf5dfe6d
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-06-13 22:51:49 +0530

    complete profile page UI

--- client/src/components/DashSideBar.jsx
+++ client/src/components/DashSideBar.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Sidebar } from "flowbite-react";
 import { HiUser, HiArrowSmRight } from "react-icons/hi";
-import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const DashSideBar = () => {
   const location = useLocation();
   const [tab, setTab] = useState("");
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
@@ -19,16 +19,16 @@ const DashSideBar = () => {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Link to="/dashboard?tab=profile">
-            <Sidebar.Item
-              active={tab === "profile"}
-              icon={HiUser}
-              label="User"
-              labelColor="dark"
-            >
-              Profile
-            </Sidebar.Item>
-          </Link>
+          <Sidebar.Item
+            as={Link}
+            to="/dashboard?tab=profile"
+            active={tab === "profile"}
+            icon={HiUser}
+            label="User"
+            labelColor="dark"
+          >
+            Profile
+          </Sidebar.Item>
           <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Sign Out
           </Sidebar.Item>
