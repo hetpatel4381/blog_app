54f53c745bdaccf9c839ef182c3c2055c8b31929
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-06-18 13:30:06 +0530

    create get-posts api route

--- api/src/routes/post.route.js
+++ api/src/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import verifyJWT from "../utils/verifyUser.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 
 const postRouter = express.Router();
 
 postRouter.post("/create", verifyJWT, createPost);
+postRouter.get("/getposts", getPosts);
 
 export default postRouter;
